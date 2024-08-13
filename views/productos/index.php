97bf504d2cd8469702ab8bdea317d164e3729b93
CUXUM <mcbrayan20@gmail.com>
2024-08-12 23:56:36 -0600

    subir

--- views/productos/index.php
+++ views/productos/index.php
@@ -3,14 +3,14 @@
     <form id="formProducto" class="border shadow p-4 col-lg-4">
         <div class="row mb-3">
             <div class="col">
-                <label for="nombre">Nombre del producto</label>
-                <input type="text" name="nombre" id="nombre" class="form-control">
+                <label for="pro_nombre">nombre del producto</label>
+                <input type="text" name="pro_nombre" id="pro_nombre" class="form-control">
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="precio">Precio del producto</label>
-                <input type="number" name="precio" id="precio" class="form-control">
+                <label for="pro_precio">precio del producto</label>
+                <input type="number" name="pro_precio" id="pro_precio" class="form-control">
             </div>
         </div>
         <div class="row">
